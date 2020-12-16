95a027667ecbad4aeafff3993d5081552929b8ad
isabeldepapel <isuchanek@gmail.com>
2020-12-16 10:44:27 -0800

    Fix day 10 part2
    Signed-off-by: isabeldepapel <isuchanek@gmail.com>

--- 2020/day10/solution.py
+++ 2020/day10/solution.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections import defaultdict
 
 def read_input(filename='input.txt'):
     with open(filename) as f:
@@ -20,17 +21,84 @@ def get_distribution(adapters):
     distribution[3] += 1
     return distribution
 
-def generate_graph(adapters):
-    graph = dict()
+def generate_graph(adapters:list):
+    graph = defaultdict(lambda:None)
+    adapters = [0] + adapters # Add 0 to the graph
+    
     i = 0
     while i < len(adapters):
         adapter = adapters[i]
+        
+        start = i + 1
+        end = i + 3
+        edges = set()
+        while start <= end and start < len(adapters):
+            if adapters[start] <= adapter + 3:
+                edges.add(adapters[start])
+            start += 1
+
+        graph[adapter] = edges
+        i += 1
+
+    # add final edge to the device (adapter + 3)
+    graph[adapters[-1]] = set([adapters[-1] + 3])
+    return graph
+
+def dfs_helper(graph:dict, node:int, memo:dict):
+    neighbors = graph[node]
+    if not neighbors:
+        return 1
+    
+    count = 0
+    for neighbor in neighbors:
+        if neighbor in memo:
+            count += memo[neighbor]
+        else:
+            count += dfs_helper(graph, neighbor, memo)
+    memo[node] = count
+    return count
+
+def dfs(graph:dict):
+    path_memo = dict()
+
+    count = dfs_helper(graph, 0, path_memo)
+    return count
+
+def get_streaks(adapters:list) -> list:
+    i = 0
+    num_streaks = []
+    streaks = []
+    adapters = [0] + adapters # Add 0 to the list
+
+    while i < len(adapters):
         start = i
-        end = i+1
+        end = i + 1
 
-        while adapter
-        graph[adapter]
+        while end < len(adapters) and adapters[end] == adapters[end-1] + 1:
+            end += 1
 
+        streaks.append(tuple(adapters[start:end]))
+        num_streaks.append(end - start)
+        i = end
+    
+    print(streaks)
+    return sorted(num_streaks)
+
+def count_arrangements(num_streaks:list) -> int:
+    # Num arrangements is the multiple of all possible combinations of the groups
+    # The number of combos in a group is equal to the sum of the preceding three combos
+    # (starting with combo length 4)
+    # Index of num_combos is the length of the streak 
+    num_combos = [1, 1, 1, 2, 4, 7]
+    num_combos_len = len(num_combos)
+
+    while num_streaks[-1] > num_combos_len:
+        num_combos.append(sum(num_combos[-3:]))
+        num_combos_len += 1
+    
+    total = reduce(lambda x,y: x*y, [num_combos[num] for num in num_streaks])
+    print(total)
+    return total
 
 def test():
     adapters = read_input('test_input.txt')
@@ -43,11 +111,24 @@ def test():
     new_dist_product = new_dist[1] * new_dist[3]
     assert new_dist_product == (22 * 10), f'product is {new_dist[1]} * {new_dist[3]}'
 
-    possible_combos = count_possible_arrangements(adapters)
-    assert possible_combos == 8, f'num combos is {possible_combos}'
+    # using combos
+    num_combos = count_arrangements(get_streaks(adapters))
+    assert num_combos == 8, f'num combos is {possible_combos}'
 
-    new_possible_combos = count_possible_arrangements(new_adapters)
-    assert new_possible_combos == 19208, f'num combos is {new_possible_combos}'
+    new_num_combos = count_arrangements(get_streaks(new_adapters))
+    assert new_num_combos == 19208, f'num combos is {new_num_combos}'
+    
+    # using dfs
+    graph = generate_graph(adapters)
+    print(graph)
+    num_combos_with_dfs = dfs(graph)
+    assert num_combos_with_dfs == 8, f'num combos is {num_combos_with_dfs}'
+
+    new_graph = generate_graph(new_adapters)
+    print(new_graph)
+    new_num_combos_with_dfs = dfs(new_graph)
+    assert new_num_combos_with_dfs == 19208, f'num combos is {new_num_combos_with_dfs}'
+
 
 def run():
     test()
@@ -57,7 +138,9 @@ def run():
     print(dist)
     print(dist[1] * dist[3])
 
-    # print(adapters)
+    print(adapters)
+    num_streaks = get_streaks(adapters)
+    count_arrangements(num_streaks)
 
 
 run()
